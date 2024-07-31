98f35b4a96e69d3d42f8acc139be19b8f9bd4168
bobbyfromgeminus <info.geminus@gmail.com>
2024-07-31 22:23:25 +0200

    20240731 #1

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# deb-lamp-wp
-Debian based LAMP server with WordPress
+# Debian based LAMP server with WordPress
+
+## 1. LAMP server
+- Run the lamp-wp.sh script file!
+- Be careful when making the necessary settings in the pop-up configuration windows!
+- If the script runs successfully, you will get a LAMP server with a pre-installed WordPress.
+
+## 2. Database and User
+- Before using WordPress, create a SQL database and a user connected to it! Use PHPMyAdmin for this!
+
+## 3. Webmin
+- To maintain and monitor your server, install the Webmin system administration application as follows:
+
+```
+curl -o setup-repos.sh https://raw.githubusercontent.com/webmin/webmin/master/setup-repos.sh
+sh setup-repos.sh
+apt-get install webmin --install-recommends -y
+```
+- After installation, you can access the webmin interface on port 10000
---
+++ lamp-wp.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# 1. UPDATE
+echo "System update -------------------------------"
+apt upgrade -y
+apt update -y
+clear
+
+# 2. APACHE
+echo "Install Apache HTTP server ------------------"
+apt install apache2 -y
+systemctl enable apache2
+systemctl restart apache2
+clear
+
+# 3. PHP
+echo "Install PHP server-side script language -----"
+apt install php php-mysql libapache2-mod-php -y
+clear
+
+# 4. DATABASE
+echo "Install MariaDB SQL database server ---------"
+apt install mariadb-server -y
+mysql_secure_installation
+clear
+
+# 5. WORDPRESS
+echo "Install WordPress content management system -"
+chown -R www-data:www-data /var/www/html
+rm /var/www/html/index.html
+cd /tmp
+wget https://wordpress.org/latest.tar.gz
+tar -xzvf latest.tar.gz
+mv ./wordpress/* /var/www/html
+systemctl restart apache2
+cd /
+clear
+
+# 6. WORDPRESS
+echo "Install PHPMyAdmin --------------------------"
+
+cd /tmp
+wget https://wordpress.org/latest.tar.gz
+apt install phpmyadmin -y
+clear
+
+echo "----------------------------------------------------"
+echo "  LAMP server succesfully installed                 "
+echo "  Installed components:                             "
+echo "    - Apache HTTP server                            "
+echo "    - PHP script language                           "
+echo "    - MariaDB SQL database                          "
+echo "    - WordPress (db and user not created)           "
+echo "    - PHPMyAdmin                                    "
+echo "                                                    "
+echo "  Before configuring WordPress,                     "
+echo "  don't forget to create a database and a SQL user! "
+echo "----------------------------------------------------"
